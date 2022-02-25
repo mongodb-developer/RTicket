6e8803c2024682be0b87b217f80a863fdeb01410
am-MongoDB <andrew.morgan@mongodb.com>
2022-02-25 18:44:07 +0000

    add TODO comment to prompt user to add the Realm appp id

--- iOS/RTicket/RTicket/RTicketApp.swift
+++ iOS/RTicket/RTicket/RTicketApp.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 import RealmSwift
 
-let realmApp = RealmSwift.App(id: "rticket-xxxxx")
+let realmApp = RealmSwift.App(id: "rticket-xxxxx") // TODO: Copy the app id from the backend Realm app
 
 @main
 struct RTicketApp: SwiftUI.App {
